d6af4404c551ed0dd2ddcf79160e179373aea67c
constROD <rialcantara21@gmail.com>
2023-01-18 04:57:00 +0800

    chore: update caching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,6 @@
 import Home from 'modules/Home';
-import { GetServerSideProps, NextPage } from 'next';
-import React from 'react';
+import { NextPage } from 'next';
 import PageLayout from 'shared/components/Layouts/PageLayout';
-
-// eslint-disable-next-line @typescript-eslint/require-await
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=60');
-
-  return {
-    props: {},
-  };
-};
 
 const HomePage: NextPage = () => {
   return (
