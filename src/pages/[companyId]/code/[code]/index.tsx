555ce917a5ce75721a799589351394557fec9ff5
drianlarde <69323240+drianlarde@users.noreply.github.com>
2024-02-09 08:30:18 +0800

    feat: add qr code page for renewme and soulscape (#4) (#5)
    * feat: add page for qr code, soulscape and renewme
    
    * chore: generate new lock file to fix deployment error
    
    * chore: use tanstack react query 4.24.4
    
    * chore: add sendgrid 7.7.0
    
    * chore: revert package.json and pnpm
    
    * chore: align vertically the bottom section
    
    * chore: add qr-code route validations
    
    * chore: update title and renew logo small
    
    * chore: revert font-bold title
    
    * chore: update text spacing
    
    * chore: add responsiveness to many screen sizes, consistent alignment
    
    * chore: update alignments, elements sizing, etc
    
    * fix: alignment
    
    * fix: alignment
    
    * fix: fix white gaps
    
    * fix: fix white gaps
    
    * chore: remove separator
    
    * chore: fit to screen hero
    
    * chore: stick vector to bottom

---
+++ src/pages/[companyId]/code/[code]/index.tsx
@@ -0,0 +1,8 @@
+import Code from 'modules/Code';
+import { NextPage } from 'next';
+
+const CodePage: NextPage = () => {
+  return <Code />;
+};
+
+export default CodePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,14 @@ module.exports = {
     './src/shared/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      xs: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+    },
     extend: {
       colors: {
         'gray-1': 'hsl(0,0%,93.3%)',
@@ -39,6 +47,8 @@ module.exports = {
         'orange-1-hover': 'hsl(15,60.5%,64.3%)',
         'green-1': 'hsl(100,35.3%,93.3%)',
         'red-1': 'hsl(300,23.5%,93.3%)',
+        'sea-1': 'hsl(182,100%,39%)',
+        'white-1': 'hsl(225,11%,93%)',
       },
     },
   },
